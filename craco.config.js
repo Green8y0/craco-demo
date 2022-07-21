9f9162e13ec1e36d56bcbd9a1b3985bbdf548ddb
weinisi <461197931@qq.com>
2022-07-21 20:35:27 +0800

    build:配置less和别名alias

---
+++ craco.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const CracoLessPlugin = require('craco-less')
+const { loaderByName } = require('@craco/craco')
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    }
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        modifyLessModuleRule(lessModuleRule, context) {
+          // Configure the file suffix
+          lessModuleRule.test = /\.module.less$/
+
+          // Configure the generated local ident name.
+          const cssLoader = lessModuleRule.use.find(loaderByName('css-loader'))
+          cssLoader.options.modules = {
+            localIdentName: '[local]_[hash:base64:5]',
+          }
+          return lessModuleRule
+        },
+        modifyLessRule(lessRule, context) {
+          // You have to exclude these file suffixes first,
+          // if you want to modify the less module's suffix
+          lessRule.exclude = /\.less$/;
+          return lessRule;
+        },
+      },
+    },
+  ]
+}
